d2df86b1e35645253ac1278aa6307024f9549f45
luiz.martins <luizgufamartins@gmail.com>
2026-01-14 11:19:24 -0300

    feat(lib/facinia): transaction list and create

--- src/clojure_finance_api/domain/repositories/transaction_repo.clj
+++ src/clojure_finance_api/domain/repositories/transaction_repo.clj
@@ -3,36 +3,35 @@
             [next.jdbc.result-set :as rs]
             [honey.sql :as sql]))
 
-(defn- column-reader [^java.sql.ResultSet rs _ ^Integer i]
-  (let [value (.getObject rs i)]
-    (if (instance? java.util.UUID value)
-      (str value) ;; Converte no ato da leitura, sem loop extra
-      value)))
-
-(def builder {:builder-fn (rs/as-maps-with-adapter
-                            rs/as-unqualified-kebab-maps
-                            column-reader)})
+(def builder {:builder-fn rs/as-unqualified-maps})
 
 (def ^:private transaction-columns
   [:id :from_user :to_user :amount :status :created_at])
+
+(def ^:private transaction-columns
+  [:t.id :t.amount :t.status :t.created_at
+   [:f.id :from_user_id] [:f.name :from_user_name] [:f.email :from_user_email]
+   [:r.id :to_user_id] [:r.name :to_user_name] [:r.email :to_user_email]])
 
 (defn find-transactions-by-user-id [ds user-id filters]
   (let [uuid (if (string? user-id) (java.util.UUID/fromString user-id) user-id)
         {:keys [type days]} filters]
     (jdbc/execute! ds
-       (sql/format
-         {:select transaction-columns
-          :from   :transactions
-          :where  [:and
-                   [:or [:= :from_user uuid] [:= :to_user uuid]]
-                   (case type
-                     "debit"  [:= :from_user uuid]
-                     "credit" [:= :to_user uuid]
-                     nil)
-                   (when days
-                     [:>= :created_at [:raw (str "now() - interval '" days " days'")]])]
-          :order-by [[:created-at :desc]]})
-       builder)))
+                   (sql/format
+                     {:select transaction-columns
+                      :from   [[:transactions :t]]
+                      :join   [[:users :f] [:= :t.from_user :f.id]
+                               [:users :r] [:= :t.to_user :r.id]]
+                      :where  [:and
+                               [:or [:= :t.from_user uuid] [:= :t.to_user uuid]]
+                               (case type
+                                 "debit"  [:= :t.from_user uuid]
+                                 "credit" [:= :t.to_user uuid]
+                                 nil)
+                               (when days
+                                 [:>= :t.created_at [:raw (str "now() - interval '" days " days'")]])]
+                      :order-by [[:t.created_at :desc]]})
+                   builder)))
 
 (defn create-transaction! [ds {:keys [from-user-id to-user-id amount]}]
   (let [amt (bigdec amount)]
--- src/clojure_finance_api/infra/graphql/schemas/types.clj
+++ src/clojure_finance_api/infra/graphql/schemas/types.clj
@@ -14,5 +14,14 @@
      :amount     {:type '(non-null Float)}
      :status     {:type 'String}
      :created_at     {:type 'String}
-     :from_user  {:type :User}
-     :to_user    {:type :User}}}})
+     :from_user  {:type :User
+                  :resolve (fn [_ _ row]
+                             {:id (:from_user_id row)
+                              :name (:from_user_name row)
+                              :email (:from_user_email row)})}
+
+     :to_user    {:type :User
+                  :resolve (fn [_ _ row]
+                             {:id (:to_user_id row)
+                              :name (:to_user_name row)
+                              :email (:to_user_email row)})}}}})
